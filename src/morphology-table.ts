fa4c72c10f819cb5f5b2d0a9a11037b6a2b6e1b0
p-adams <ghusiyy@gmail.com>
2021-07-08 18:14:09 -0700

    init

---
+++ src/morphology-table.ts
@@ -0,0 +1,9 @@
+import { html, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+
+@customElement("morphology-table")
+export class MorphologyTable extends LitElement {
+  render() {
+    return html`<div>morphology table</div>`;
+  }
+}
