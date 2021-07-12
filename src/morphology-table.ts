f184f45b0133977261cd07da2d530bb088f73790
p-adams <ghusiyy@gmail.com>
2021-07-11 23:26:29 -0700

    impl for table w/column headers

--- src/morphology-table.ts
+++ src/morphology-table.ts
@@ -1,12 +1,32 @@
-import { html, LitElement } from "lit";
+import { css, html, LitElement } from "lit";
 import { customElement, property } from "lit/decorators.js";
 
 @customElement("morphology-table")
 export class MorphologyTable extends LitElement {
+  static styles = css`
+    .table-container {
+      display: grid;
+      grid-template-columns: 75px auto auto auto;
+    }
+    .cell-header {
+      border: 1px solid lightgray;
+      text-align: center;
+    }
+  `;
   @property()
   root: String = "فعل";
   tense: "past" | "present" | "future" = "past";
   render() {
-    return html`<div>morphology table</div>`;
+    return html`<div class="table-container">
+      <div class="cell-header empty"></div>
+      <div class="cell-header">plural</div>
+      <div class="cell-header">dual</div>
+      <div
+        class="
+        cell-header"
+      >
+        singular
+      </div>
+    </div>`;
   }
 }
